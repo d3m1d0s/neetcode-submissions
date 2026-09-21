1c431d21acf11902f31ba78728a2a3c86dabef87
Demid Ostiakov <demid.ostiakov@gmail.com>
2026-09-22 01:01:44 +0200

    Add: minimum-stack - submission-14

---
+++ Data Structures & Algorithms/minimum-stack/submission-14.java
@@ -0,0 +1,65 @@
+
+class MinStack {
+
+    private int[] array;
+    private int capacity = 0;
+    private int length = 0;
+    private int[] minArray;
+    private int lengthMin = 0;
+
+    public MinStack() {
+        
+    }
+
+    public void printArray(int[] array) {
+        for (int el : array) {
+            System.out.print(el + " ");
+        }
+        System.out.println();
+    }
+    
+    public void push(int val) {
+
+        if (length == 0) {
+            array = new int[2];
+            capacity = array.length;
+        }
+        if (lengthMin == 0) {
+            minArray = new int[2];
+        }
+        if (length == capacity) {
+            int[] old_array = array;
+            int[] old_minArray = minArray;
+            array = new int[capacity * 2];
+            minArray = new int[capacity * 2];
+            for (int i = 0; i < capacity; i++) {
+                array[i] = old_array[i];
+                minArray[i] = old_minArray[i];
+            }
+            capacity = array.length;
+        }
+        array[length] = val;
+        length++;
+        
+        if (lengthMin == 0 || val <= minArray[lengthMin - 1]) {
+            //printArray(minArray);
+            minArray[lengthMin] = val;
+            lengthMin++;
+        }
+    }
+    
+    public void pop() {
+        if (array[length-1] == minArray[lengthMin - 1]) {
+            lengthMin--;
+        }
+        length--;
+    }
+    
+    public int top() {
+        return array[length - 1];
+    }
+    
+    public int getMin() {
+        return minArray[lengthMin - 1];
+    }
+}
